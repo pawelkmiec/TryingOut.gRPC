efb526769a4e9d1952c9441e6d0f4fc1a0cfa8f8
pawel.kmiec <pj.kmiec@gmail.com>
2019-10-18 12:52:02 +0200

    chore: consuming default gRPC service

---
+++ TryingOut.gRPC.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using TryingOut.gRPC.Service;
+
+namespace TryingOut.gRPC.Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "Paweł"
+            });
+            Console.WriteLine($"Response from server: {response.Message}");
+        }
+    }
+}
