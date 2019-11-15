2c5391daef8962750b9362351ee7805a090157c6
pawel.kmiec <pj.kmiec@gmail.com>
2019-11-15 15:27:47 +0100

    remove benchmark projects

--- TryingOut.Benchmark.Contracts/AuthorDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TryingOut.Benchmark.Contracts
-{
-    public class AuthorDto
-    {
-        public string FirstName { get; set; }
-        
-        public string LastName { get; set; }
-    }
-}
